c196f46e319a6186046d427e6ee5416a9cc2c5a8
Ali Abbas <itsgeniusstar@gmail.com>
2014-12-28 01:10:54 +0500

    Use side

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+if (!ini_get('date.timezone')) {
+    date_default_timezone_set('GMT');
+}
 error_reporting(E_ALL);
 // change the following paths if necessary
 $yii = dirname(__FILE__) . '/../../frameworks/yii-1.1.15.022a51/framework/yii.php';
