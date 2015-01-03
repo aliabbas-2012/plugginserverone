be9328e5702ef76955392789be0c948350b14e96
Ali Abbas <itsgeniusstar@gmail.com>
2015-01-04 00:24:14 +0500

    sigin up

--- themes/abound/views/api/purchase_notification.php
+++ themes/abound/views/api/purchase_notification.php
@@ -1,7 +1,7 @@
 <h3>Your Trail period has over. You need to upgrade your plugin!</h3>
 
 <label for="signup">Not Already registered ? </label>
-<a href="<?php echo $this->createAbsoluteUrl('/web/users/singup',array()); ?>" target="_blank" title="Click to Sign Up">Sing Up</a>
+<a href="<?php echo $this->createAbsoluteUrl('/web/users/register',array()); ?>" target="_blank" title="Click to Sign Up">Sing Up</a>
 
 OR
 
